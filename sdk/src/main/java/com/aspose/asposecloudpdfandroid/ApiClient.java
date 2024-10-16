5e3efd34e42e7d3011e451bdbefb7ac4ee57a339
kirill.novinskiy@aspose.com <kirill.novinskiy@aspose.com>
2024-10-16 14:22:30 +0300

    update to 24.10

--- sdk/src/main/java/com/aspose/asposecloudpdfandroid/ApiClient.java
+++ sdk/src/main/java/com/aspose/asposecloudpdfandroid/ApiClient.java
@@ -94,7 +94,7 @@ public class ApiClient {
         
         // Add custom headers
         addDefaultHeader("x-aspose-client", "android sdk");
-        addDefaultHeader("x-aspose-client-version", "24.9.0");
+        addDefaultHeader("x-aspose-client-version", "24.10.0");
         
         // PDFCLOUD-418 Set default Connect Timeout
         setConnectTimeout(5 * 60 * 1000);
